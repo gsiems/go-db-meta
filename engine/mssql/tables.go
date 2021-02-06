e2c157f27db785b1f67dbd67d011dce4a868e4bb
Gregory Siems <gsiems@gmail.com>
2021-02-06 14:43:45 -0600

    Where clause tweak

--- engine/mssql/tables.go
+++ engine/mssql/tables.go
@@ -29,6 +29,7 @@ SELECT tab.table_catalog,
             AND v.table_name = tab.table_name )
     OUTER APPLY ::fn_listextendedproperty ( 'MS_Description', 'schema', tab.table_schema, 'table', tab.table_name ) xp
     WHERE tab.table_schema NOT IN ( 'INFORMATION_SCHEMA', 'sys' )
+        AND substring ( tab.table_schema, 1, 3 ) <> 'db_'
         AND substring ( tab.table_name, 1, 1 ) <> '#'
         AND ( tab.table_schema = args.schema_name
             OR args.schema_name = '' )
