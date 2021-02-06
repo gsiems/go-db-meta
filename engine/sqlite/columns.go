63e8ec19165b7f3bb54d9c0b915d21ecb7a54b9d
Gregory Siems <gsiems@gmail.com>
2021-02-06 08:34:05 -0600

    rewrite as a single database query

--- engine/sqlite/columns.go
+++ engine/sqlite/columns.go
@@ -7,6 +7,50 @@ import (
 	m "github.com/gsiems/go-db-meta/model"
 )
 
+// Columns obtains the list of columns
+// for the (tableSchema, tableName) parameters and returns the results
+func Columns(db *m.DB, tableSchema, tableName string) ([]m.Column, error) {
+
+	var d []m.Column
+
+	q := `
+SELECT '%s' AS table_catalog,
+        args.table_schema,
+        m.name AS table_name,
+        cols.name AS column_name,
+        cols.cid AS ordinal_position,
+        CASE
+            WHEN cols."notnull" = 1 THEN 'N'
+            ELSE 'Y'
+            END AS is_nullable,
+        cols."type" AS data_type,
+        NULL AS domain_catalog,
+        NULL AS domain_schema,
+        NULL AS domain_name,
+        NULL AS comment
+    FROM sqlite_master AS m
+    JOIN pragma_table_info ( m.name ) AS cols
+    CROSS JOIN (
+        SELECT coalesce ( $1, '' ) AS table_schema,
+                coalesce ( $2, '' ) AS table_name
+        ) AS args
+    WHERE m.type IN ( 'table', 'view' )
+        AND m.tbl_name NOT LIKE 'sqlite_%'
+        AND ( args.table_name = '' OR args.table_name = m.name )
+`
+
+	catName, err := catalogName(db)
+	if err != nil {
+		return d, err
+	}
+
+	q2 = fmt.Sprintf(q, catName)
+	return db.Columns(q2, tableSchema, tableName)
+}
+
+
+
+/*
 type pto struct {
 	cid        sql.NullInt32
 	colName    sql.NullString
@@ -46,7 +90,7 @@ func Columns(db *m.DB, tableSchema, tableName string) ([]m.Column, error) {
 
 func tableColumns(db *m.DB, tableSchema, tableName string) ([]m.Column, error) {
 
-	var d []Column
+	var d []m.Column
 
 	catName, err := catalogName(db)
 	if err != nil {
@@ -57,8 +101,8 @@ func tableColumns(db *m.DB, tableSchema, tableName string) ([]m.Column, error) {
 SELECT "cid" AS ordinal_position,
         "name" AS column_name,
         "type" AS dataType,
-        "notnull" AS not_null
-        dflt_value AS default_value,
+        "notnull" AS not_null,
+        dflt_value AS default_value
     FROM pragma_table_info('%s')
     ORDER BY cid
 `
@@ -85,7 +129,7 @@ SELECT "cid" AS ordinal_position,
 			return d, err
 		} else {
 
-			var u Column
+			var u m.Column
 
 			u.TableCatalog = catName
 			u.TableSchema = schemaName
@@ -107,3 +151,4 @@ SELECT "cid" AS ordinal_position,
 
 	return d, err
 }
+*/
