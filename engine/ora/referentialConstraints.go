254d8ffed9aaf4ce2b04711faad3dd45aab13024
Gregory Siems <gsiems@gmail.com>
2021-02-08 15:44:31 -0600

    Add referential constraints

---
+++ engine/ora/referentialConstraints.go
@@ -0,0 +1,63 @@
+package ora
+
+import (
+	m "github.com/gsiems/go-db-meta/model"
+)
+
+// ReferentialConstraints defines the query for obtaining the
+// referential constraints for the (tableSchema, tableName) parameters
+// (as either the parent or child) and returns the results of executing
+// the query
+func ReferentialConstraints(db *m.DB, tableSchema, tableName string) ([]m.ReferentialConstraint, error) {
+
+	q := `
+WITH args AS (
+    SELECT $1 AS schema_name,
+            $2 AS table_name
+)
+SELECT sys_context ( 'userenv', 'DB_NAME' ) AS table_catalog,
+        con.owner AS table_schema,
+        con.table_name,
+        listagg ( col.column_name, ', ' ) WITHIN GROUP (
+            ORDER BY col.position ) AS table_columns,
+        --con.constraint_type,
+        con.constraint_name,
+        sys_context ( 'userenv', 'DB_NAME' ) AS ref_table_catalog,
+        rcon.owner AS ref_table_schema,
+        rcon.table_name AS ref_table_name,
+        listagg ( rcol.column_name, ', ' ) WITHIN GROUP (
+            ORDER BY rcol.position ) AS ref_table_columns,
+        --rcon.constraint_type AS ref_constraint_type,
+        rcon.constraint_name AS ref_constraint_name,
+        NULL AS match_option, -- TODO
+        con.delete_rule,
+        'RESTRICT' AS update_rule,
+        CASE con.status
+            WHEN 'ENABLED' THEN 'YES'
+            WHEN 'DISABLED' THEN 'NO'
+            ELSE con.status
+            END AS is_enforced,
+        --con.deferrable AS is_deferrable,
+        --con.deferred AS initially_deferred,
+        NULL AS comments
+    FROM dba_constraints con
+    JOIN dba_constraints rcon
+        ON ( con.r_owner = rcon.owner
+            AND con.r_constraint_name = rcon.constraint_name )
+    CROSS JOIN args
+    JOIN dba_cons_columns col
+        ON ( con.owner = col.owner
+            AND con.constraint_name = col.constraint_name
+            AND con.table_name = col.table_name )
+    JOIN dba_cons_columns rcol
+        ON ( rcon.owner = rcol.owner
+            AND rcon.constraint_name = rcol.constraint_name
+            AND rcon.table_name = rcol.table_name )
+    WHERE con.constraint_type = 'R'
+        AND ( ( ( con.owner = args.schema_name OR ( args.schema_name = '' AND args.table_name = '' ) )
+                AND ( con.table_name = args.table_name OR args.table_name = '' ) )
+            OR ( ( rcon.owner = args.schema_name OR ( args.schema_name = '' AND args.table_name = '' ) )
+                AND ( rcon.table_name = args.table_name OR args.table_name = '' ) ) )
+`
+	return db.ReferentialConstraints(q, tableSchema, tableName)
+}
---
+++ model/referentialConstraints.go
@@ -0,0 +1,118 @@
+package model
+
+import (
+	"database/sql"
+	"errors"
+)
+
+/*
+
+| Table Name                            | Column Name                       | Position | Matches                                 | Qty |
+| ------------------------------------- | --------------------------------- | -------- | --------------------------------------- | --- |
+| REFERENTIAL_CONSTRAINTS               | CONSTRAINT_CATALOG                | 1        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | CONSTRAINT_SCHEMA                 | 2        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | CONSTRAINT_NAME                   | 3        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | UNIQUE_CONSTRAINT_CATALOG         | 4        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | UNIQUE_CONSTRAINT_SCHEMA          | 5        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | UNIQUE_CONSTRAINT_NAME            | 6        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | MATCH_OPTION                      | 7        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | UPDATE_RULE                       | 8        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| REFERENTIAL_CONSTRAINTS               | DELETE_RULE                       | 9        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+
+| CONSTRAINT_COLUMN_USAGE               | TABLE_CATALOG                     | 1        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | TABLE_SCHEMA                      | 2        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | TABLE_NAME                        | 3        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | COLUMN_NAME                       | 4        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | CONSTRAINT_CATALOG                | 5        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | CONSTRAINT_SCHEMA                 | 6        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_COLUMN_USAGE               | CONSTRAINT_NAME                   | 7        | sql2003, pg, mssql, hsqldb              | 4   |
+
+| CONSTRAINT_TABLE_USAGE                | TABLE_CATALOG                     | 1        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_TABLE_USAGE                | TABLE_SCHEMA                      | 2        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_TABLE_USAGE                | TABLE_NAME                        | 3        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_TABLE_USAGE                | CONSTRAINT_CATALOG                | 4        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_TABLE_USAGE                | CONSTRAINT_SCHEMA                 | 5        | sql2003, pg, mssql, hsqldb              | 4   |
+| CONSTRAINT_TABLE_USAGE                | CONSTRAINT_NAME                   | 6        | sql2003, pg, mssql, hsqldb              | 4   |
+
+| KEY_COLUMN_USAGE                      | CONSTRAINT_CATALOG                | 1        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | CONSTRAINT_SCHEMA                 | 2        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | CONSTRAINT_NAME                   | 3        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | TABLE_CATALOG                     | 4        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | TABLE_SCHEMA                      | 5        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | TABLE_NAME                        | 6        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | COLUMN_NAME                       | 7        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | ORDINAL_POSITION                  | 8        | sql2003, pg, mssql, mariadb, hsqldb, h2 | 6   |
+| KEY_COLUMN_USAGE                      | POSITION_IN_UNIQUE_CONSTRAINT     | 9        | sql2003, pg, mariadb, hsqldb, h2        | 5   |
+| KEY_COLUMN_USAGE                      | REFERENCED_COLUMN_NAME            | (null)   | mariadb                                 | 1   |
+| KEY_COLUMN_USAGE                      | REFERENCED_TABLE_NAME             | (null)   | mariadb                                 | 1   |
+| KEY_COLUMN_USAGE                      | REFERENCED_TABLE_SCHEMA           | (null)   | mariadb                                 | 1   |
+
+*/
+
+// ReferentialConstraint contains details for referential constraints
+type ReferentialConstraint struct {
+	TableCatalog      sql.NullString `json:"tableCatalog"`
+	TableSchema       sql.NullString `json:"tableSchema"`
+	TableName         sql.NullString `json:"tableName"`
+	TableColumns      sql.NullString `json:"tableColumns"`
+	ConstraintName    sql.NullString `json:"constraintName"`
+	RefTableCatalog   sql.NullString `json:"refTableCatalog"`
+	RefTableSchema    sql.NullString `json:"refTableSchema"`
+	RefTableName      sql.NullString `json:"refTableName"`
+	RefTableColumns   sql.NullString `json:"refTableColumns"`
+	RefConstraintName sql.NullString `json:"refConstraintName"`
+	MatchOption       sql.NullString `json:"matchOption"`
+	UpdateRule        sql.NullString `json:"updateRule"`
+	DeleteRule        sql.NullString `json:"deleteRule"`
+	IsEnforced        sql.NullString `json:"isEnforced"`
+	//is_deferrable
+	//initially_deferred
+	Comment sql.NullString `json:"comment"`
+}
+
+// Constraints returns a slice of Referential Constraints for the (schema) parameter
+func (db *m.DB) ReferentialConstraints(q, schema string) ([]ReferentialConstraint, error) {
+
+	var d []ReferentialConstraint
+
+	if q == "" {
+		return d, errors.New("No query provided to ReferentialConstraints")
+	}
+
+	rows, err := db.Query(q, schema)
+	if err != nil {
+		return d, err
+	}
+	defer func() {
+		if cerr := rows.Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}()
+
+	for rows.Next() {
+		var u ReferentialConstraint
+		err = rows.Scan(&u.TableCatalog,
+			&u.TableSchema,
+			&u.TableName,
+			&u.TableColumns,
+			&u.ConstraintName,
+			&u.RefTableCatalog,
+			&u.RefTableSchema,
+			&u.RefTableName,
+			&u.RefTableColumns,
+			&u.RefConstraintName,
+			&u.MatchOption,
+			&u.UpdateRule,
+			&u.DeleteRule,
+			&u.IsEnforced,
+			&u.Comment,
+		)
+		if err != nil {
+			return d, err
+		} else {
+			d = append(d, u)
+		}
+	}
+
+	return d, err
+}
