23d5ae4dd3345f01ea8d6d076d0415d32340257f
Gregory Siems <gsiems@gmail.com>
2021-12-22 23:37:14 -0600

    update list of system schemas

--- engine/ora/main.go
+++ engine/ora/main.go
@@ -1,9 +1,43 @@
 package ora
 
+/* Would that Oracle had taken a page from the PostgreSQL book and done
+something like making ORA_ a reserved prefix and then used it consistently for
+naming the DB system schemas/users...
+
+The following does appear to catch most, maybe all, of them for versions > 11?
+
+SELECT du.username
+    FROM dba_users du
+    WHERE du.oracle_maintained = 'Y'
+        OR EXISTS (
+            SELECT 1
+                FROM sys.ku_noexp_tab s
+                WHERE s.obj_type = 'SCHEMA'
+                    AND s.name = du.username
+        )
+        OR EXISTS (
+            SELECT 1
+                FROM system.logstdby$skip_support s
+                WHERE s.action IN ( 0, -1 )
+                    AND s.name = du.username
+        )
+        OR EXISTS (
+            SELECT 1
+                FROM v$sysaux_occupants s
+                WHERE s.schema_name = du.username
+        )
+;
+
+*/
+
 const systemTables = `
-    'APPQOSSYS', 'AWR_STAGE', 'CSMIG', 'CTXSYS', 'DBSNMP', 'DIP',
-    'DMSYS', 'DSSYS', 'EXFSYS', 'LBACSYS', 'MDSYS', 'OLAPSYS',
-    'ORACLE_OCM', 'ORDPLUGINS', 'ORDSYS', 'OUTLN', 'PERFSTAT',
-    'PUBLIC', 'SQLTXPLAIN', 'SYS', 'SYSMAN', 'SYSTEM', 'TRACESVR',
-    'TSMSYS', 'WMSYS', 'XDB'
+    'ANONYMOUS', 'APEX_190200', 'APEX_PUBLIC_USER', 'APPQOSSYS',
+    'AUDSYS', 'AWR_STAGE', 'CSMIG', 'CTXSYS', 'DBSFWUSER', 'DBSNMP',
+    'DIP', 'DMSYS', 'DSSYS', 'EXFSYS', 'FLOWS_FILES', 'GGSYS',
+    'GSMADMIN_INTERNAL', 'GSMCATUSER', 'GSMUSER', 'LBACSYS', 'MDSYS',
+    'OJVMSYS', 'OLAPSYS', 'ORACLE_OCM', 'ORDPLUGINS', 'ORDSYS',
+    'OUTLN', 'PERFSTAT', 'PUBLIC', 'REMOTE_SCHEDULER_AGENT',
+    'SQLTXPLAIN', 'SYS', 'SYSBACKUP', 'SYSDG', 'SYSKM', 'SYSMAN',
+    'SYSRAC', 'SYSTEM', 'SYS$UMF', 'TRACESVR', 'TSMSYS', 'WMSYS',
+    'XDB', 'XS$NULL'
 `
