082605de0b8d33bcbb3c9e83d41428c57a2bbbf6
Gregory Siems <gsiems@gmail.com>
2025-12-24 22:05:59 -0600

    Filter out indexes that are being dropped, tweaks for (hopefully) better readability).

--- dbms/pg/indexes.go
+++ dbms/pg/indexes.go
@@ -13,53 +13,53 @@ func Indexes(db *sql.DB, schemaName, tableName string) ([]m.Index, error) {
 
 	q := `
 WITH args AS (
-    SELECT coalesce ( $1, '' ) AS schema_name,
-            coalesce ( $2, '' ) AS table_name
+    SELECT current_database () AS db_name,
+            coalesce ( $1, '' ) AS schema_name,
+            coalesce ( $2, '' ) AS table_name,
+            coalesce ( $1, $2, '' ) = '' AS ignore_schema,
+            coalesce ( $2, '' ) = '' AS ignore_table
 ),
 idx AS (
-   SELECT current_database () AS index_catalog,
-           nr.nspname AS index_schema,
-           c2.relname AS index_name,
-           trim ( split_part ( regexp_replace ( pg_catalog.pg_get_indexdef ( i.indexrelid, 0, true ), '^[^\(]+USING ', '' ), '(', 1 ) ) AS index_type,
-           regexp_replace ( pg_catalog.pg_get_indexdef ( i.indexrelid, 0, true ), '^[^\(]+', '' ) AS index_columns,
-           current_database () AS table_catalog,
-           nr.nspname AS table_schema,
-           c.relname AS table_name,
-           CASE
-               WHEN i.indisunique THEN 'YES'
-               ELSE 'NO'
-               END AS is_unique,
-           d.description AS comments
-       FROM pg_catalog.pg_class c
-       CROSS JOIN args
-       INNER JOIN pg_catalog.pg_index i
-           ON ( c.oid = i.indrelid )
-       INNER JOIN pg_catalog.pg_class c2
-           ON ( i.indexrelid = c2.oid )
-       LEFT OUTER JOIN pg_catalog.pg_description d
-           ON ( d.objoid = i.indexrelid )
-       INNER JOIN pg_namespace nr
-           ON ( nr.oid = c.relnamespace )
-       WHERE nr.nspname <> 'information_schema'
-           AND nr.nspname !~ '^pg_'
-           AND ( nr.nspname = args.schema_name OR ( args.schema_name = '' AND args.table_name = '' ) )
-           AND ( c.relname = args.table_name OR args.table_name = '' )
+    SELECT nr.nspname::text AS index_schema,
+            c2.relname::text AS index_name,
+            nr.nspname::text AS table_schema,
+            c.relname::text AS table_name,
+            i.indisunique,
+            d.description AS comments,
+            regexp_split_to_array (
+                split_part ( pg_catalog.pg_get_indexdef ( i.indexrelid, 0, true ), 'INDEX', 2 ),
+                '[\(\)]' ) AS def
+        FROM pg_catalog.pg_index i
+        CROSS JOIN args
+        INNER JOIN pg_catalog.pg_class c
+            ON ( c.oid = i.indrelid )
+        INNER JOIN pg_catalog.pg_class c2
+            ON ( c2.oid = i.indexrelid )
+        LEFT OUTER JOIN pg_catalog.pg_description d
+            ON ( d.objoid = i.indexrelid )
+        INNER JOIN pg_namespace nr
+            ON ( nr.oid = c.relnamespace )
+        WHERE i.indislive
+            AND nr.nspname <> 'information_schema'
+            AND nr.nspname !~ '^pg_'
+            AND ( nr.nspname = args.schema_name OR args.ignore_schema )
+            AND ( c.relname = args.table_name OR args.ignore_table )
 )
-SELECT index_catalog,
-        index_schema,
-        index_name,
-        index_type,
+SELECT args.db_name AS index_catalog,
+        idx.index_schema,
+        idx.index_name,
+        split_part ( idx.def[1], ' ', 6 ) AS index_type,
+        idx.def[2] AS index_columns,
+        args.db_name AS table_catalog,
+        idx.table_schema,
+        idx.table_name,
         CASE
-            WHEN substr ( index_columns, length ( index_columns ), 1 ) = ')'
-                THEN substr ( index_columns, 2, length ( index_columns ) - 2 )
-            ELSE index_columns
-            END AS index_columns,
-        table_catalog,
-        table_schema,
-        table_name,
-        is_unique,
+           WHEN idx.indisunique THEN 'YES'
+           ELSE 'NO'
+           END AS is_unique,
         comments
     FROM idx
+    CROSS JOIN args
 `
 	return m.Indexes(db, q, schemaName, tableName)
 }
